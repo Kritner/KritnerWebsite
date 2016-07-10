d67d74be5cdae9ebb9a90d6883b66b43ca793e2e
Russ Hammett <Kritner@gmail.com>
2016-07-09 22:32:53 -0400

    -Added active navigation link -downgraded jquery as latest version doesn't work with bootstrap -added new testing area to house my ... test stuff.

--- src/KritnerWebsite/wwwroot/js/site.js
+++ src/KritnerWebsite/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your Javascript code.
+
+// Add active class to active navigation link
+$(document).ready(function () {
+    $('ul.nav.navbar-nav').find('a[href="' + location.pathname + '"]')
+        .closest('li').addClass('active');
+});
