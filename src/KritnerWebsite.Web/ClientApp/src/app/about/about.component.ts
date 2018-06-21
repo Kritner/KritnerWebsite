aa6300c43e53a8035d3de4807e0aed100a187c84
Russ Hammett <russ.hammett@g2-inc.com>
2018-06-21 14:30:40 -0400

    Additional about component changes, reformatting Removed stack exchange banner from nav, added to about

--- src/KritnerWebsite.Web/ClientApp/src/app/about/about.component.ts
+++ src/KritnerWebsite.Web/ClientApp/src/app/about/about.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
 }
