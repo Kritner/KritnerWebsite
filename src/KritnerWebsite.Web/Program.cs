0aa20f2bea134a30b89174af80b73e40c23c55b6
Russ Hammett <russ.hammett@g2-inc.com>
2018-06-23 08:52:53 -0400

    Adds kestral support

--- src/KritnerWebsite.Web/Program.cs
+++ src/KritnerWebsite.Web/Program.cs
@@ -19,6 +19,8 @@ namespace KritnerWebsite.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000", "https://*:5001")
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
