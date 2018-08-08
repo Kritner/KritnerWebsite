6e8c88c2b9710cf12cfac5b00c8f56bff17a718d
Russ Hammett <kritner@gmail.com>
2018-08-08 17:31:09 -0400

    Initial pass at projection, still writing tests but ran out of time.

---
+++ src/KritnerWebsite.Business.Tests/Services/ProjectFutureEnergyCostServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using KritnerWebsite.Business.Models;
+using KritnerWebsite.Business.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KritnerWebsite.Business.Tests.Services
+{
+    [TestFixture]
+    public class ProjectFutureEnergyCostServiceTests
+    {
+        private readonly ProjectFutureEnergyCostService _subject = new ProjectFutureEnergyCostService();
+        private const int ORIGINAL_KWH = 1000;
+        private const int PERCENT_INCREASE_PER_YEAR = 10;
+        private const decimal ORIGINAL_COST = 100;
+        
+        [Test]
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void ShouldProjectCorrectNumberOfYears(int yearsToProject)
+        {
+            var result = _subject.CalculateFutureProjection(
+                GetSampleData(), 
+                new ProjectionParameters(GetSampleData(), yearsToProject, PERCENT_INCREASE_PER_YEAR)
+            );
+
+            Assert.AreEqual(yearsToProject, result.BgeFutureProjection.Count, nameof(yearsToProject));
+        }
+
+        [Test]
+        [TestCase(5)]
+        public void ShouldIncreaseProjectionByCorrectAmount(int yearsToProject)
+        {
+            var result = _subject.CalculateFutureProjection(
+                GetSampleData(),
+                new ProjectionParameters(GetSampleData(), yearsToProject, PERCENT_INCREASE_PER_YEAR)
+            );
+
+            var sample = GetSampleData();
+            Assert.AreEqual(sample.AverageCostPerMonth, result.BgeFutureProjection[0].AverageCostPerMonth, $"Index 0 should be unchanged");
+            for (int i = 1; i < yearsToProject; i++)
+            {
+                // TODO
+                throw new NotImplementedException()
+            }
+        }
+
+        private static YearlyElectricityUsage GetSampleData()
+        {
+            return new YearlyElectricityUsage(new List<MonthlyElectrictyUsage>()
+            {
+                new MonthlyElectrictyUsage(new DateTime(), ORIGINAL_KWH, ORIGINAL_COST)
+            });
+        }
+    }
+}
--- src/KritnerWebsite.Business/Services/ProjectFutureEnergyCostService.cs
+++ src/KritnerWebsite.Business/Services/ProjectFutureEnergyCostService.cs
@@ -7,19 +7,34 @@ namespace KritnerWebsite.Business.Services
 {
     public class ProjectFutureEnergyCostService
     {
-        public SolarBgeProjection CalculateFutureProjection(YearlyElectricityUsage solarEstimate, ProjectionParameters projectionParameters)
+        public SolarBgeProjection CalculateFutureProjection(
+            YearlyElectricityUsage solarEstimate, 
+            ProjectionParameters projectionParameters
+        )
         {
+            var projection = new Dictionary<int, YearlyElectricityUsage>();
             var hold = projectionParameters.OriginalYearlyUsage;
-            for (int i = 0; i < projectionParameters.YearsToProject; i++)
+            projection.Add(0, hold);
+
+            // Each year to project
+            for (int i = 1; i < projectionParameters.YearsToProject; i++)
             {
-                foreach (var item in hold.MonthlyUsage)
+                var monthlyUsage = new List<MonthlyElectrictyUsage>();
+                // for each month, create a new month multiplied by the percent increase
+                foreach (var month in hold.MonthlyUsage)
                 {
-                    // TODO: the thing
-                    throw new NotImplementedException();
+                    monthlyUsage.Add(new MonthlyElectrictyUsage(
+                        month.BillingPeriodStart, 
+                        month.KiloWattHours, 
+                        month.Cost * projectionParameters.PercentIncreasePerYear
+                    ));
                 }
+
+                hold = new YearlyElectricityUsage(monthlyUsage);
+                projection.Add(i, hold);
             }
 
-            return new SolarBgeProjection(solarEstimate, null);
+            return new SolarBgeProjection(solarEstimate, projection);
         }
     }
 }
