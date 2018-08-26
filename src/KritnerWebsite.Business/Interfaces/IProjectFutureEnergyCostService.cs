f5c48ef47f0c189f702bd36e519ae28c8eb101de
Russ Hammett <Kritner@gmail.com>
2018-08-26 11:49:02 -0400

    Revert "Projected solar (#10)"
    This reverts commit 5b47a3a12044f83273a2f7af51df0f40c1572087.

--- src/KritnerWebsite.Business/Interfaces/IProjectFutureEnergyCostService.cs
+++
@@ -1,9 +0,0 @@
-﻿using KritnerWebsite.Business.Models;
-
-namespace KritnerWebsite.Business.Interfaces
-{
-    public interface IProjectFutureEnergyCostService
-    {
-        SolarVsUtilityProjection CalculateFutureProjection(IYearlyKwhUsage solarEstimate, ProjectionParameters projectionParameters);
-    }
-}
