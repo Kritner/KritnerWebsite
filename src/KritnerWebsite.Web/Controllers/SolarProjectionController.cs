d6ba5e821ff00cccd198e87e72d0ed471c5edae3
Russ Hammett <Kritner@gmail.com>
2018-09-08 18:44:09 -0400

    Documentation update, and checking that the failing hook check is removed.

--- src/KritnerWebsite.Web/Controllers/SolarProjectionController.cs
+++ src/KritnerWebsite.Web/Controllers/SolarProjectionController.cs
@@ -36,25 +36,38 @@ namespace KritnerWebsite.Web.Controllers
             );
         }
 
+        /// <summary>
+        /// Get solar projection with a json 
+        /// string representing <see cref="SolarProjectionParameters"/>.
+        /// 
+        /// TODO there's gotta be a better way to do this signature,
+        /// both from a param and return perspective.  I feel like
+        /// parameter should convey the proper type, and the result
+        /// should be able to convey a status perhaps?
+        /// </summary>
+        /// <param name="param">Json representation of <see cref="SolarProjectionParameters"/></param>
+        /// <returns><see cref="SolarVsUtilityProjection"/></returns>
         [HttpGet]
         public SolarVsUtilityProjection Get(string param)
         {
+            // Convert json string to type
             var solarProjectionParameters = JsonConvert
                 .DeserializeObject<SolarProjectionParameters>(param);
-                
+            
+            // Calculate future projection
             return _service.CalculateFutureProjection(
-                new YearlyKwhUsageFromAnnual(
-                    solarProjectionParameters.SolarCostPerMonth * 12,
-                    solarProjectionParameters.UtilitySolarArrayKwhYear
+                solarEstimate: new YearlyKwhUsageFromAnnual(
+                    totalCost: solarProjectionParameters.SolarCostPerMonth * 12,
+                    totalKiloWattHours: solarProjectionParameters.UtilitySolarArrayKwhYear
                 ),
-                new ProjectionParameters(
-                    new YearlyKwhUsageFromAnnual(
-                        solarProjectionParameters.UtilityCostFullYear, 
-                        solarProjectionParameters.UtilitySolarArrayKwhYear
+                projectionParameters: new ProjectionParameters(
+                    utilityYear: new YearlyKwhUsageFromAnnual(
+                        totalCost: solarProjectionParameters.UtilityCostFullYear,
+                        totalKiloWattHours: solarProjectionParameters.UtilitySolarArrayKwhYear
                     ),
-                    solarProjectionParameters.YearsToProject,
-                    solarProjectionParameters.SolarFinanceYears,
-                    solarProjectionParameters.UtilityPercentIncreasePerYear
+                    yearsToProject: solarProjectionParameters.YearsToProject,
+                    financeYears: solarProjectionParameters.SolarFinanceYears,
+                    percentIncreasePerYear: solarProjectionParameters.UtilityPercentIncreasePerYear
                 )
             );
         }
